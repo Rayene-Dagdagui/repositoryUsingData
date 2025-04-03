ceeeabc6f708a2b0f4499b37331b954fa4f804d7
Rayene Dagdagui <dagdagui.rayene@iisgalvanimi.edu.it>
2025-04-03 21:48:19 +0000

    lavoro using data 3 aprile 2025

---
+++ angular/src/app/user-item/user-item.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-item',
+  imports: [],
+  templateUrl: './user-item.component.html',
+  styleUrl: './user-item.component.css'
+})
+export class UserItemComponent {
+  name: string; // <-- aggiunta la proprietà name
+  constructor() { 
+    this.name = 'Felipe'; // setta il nome nel costruttore
+  }
+}
