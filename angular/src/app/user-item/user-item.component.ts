adff490279907151e2d1a76d633f9fb533cd0c3c
Rayene Dagdagui <dagdagui.rayene@iisgalvanimi.edu.it>
2025-04-10 11:59:25 +0000

    fine lavoro 10 aprile 2025

--- angular/src/app/user-item/user-item.component.ts
+++ angular/src/app/user-item/user-item.component.ts
@@ -1,15 +1,15 @@
 import { Component, Input } from '@angular/core';
-
+//  aggiungi input agli imports
 @Component({
   selector: 'app-user-item',
-  imports: [Input], // <--- aggiungi questo
+  imports: [], 
  
   templateUrl: './user-item.component.html',
   styleUrl: './user-item.component.css'
 })
 export class UserItemComponent {
   //name: string; // <-- aggiunta la proprietà name
-  @Input() name: string = ""; // <-- Aggiungi la notazione @input
+  @Input() name: string; // <-- Aggiungi la notazione @input
   constructor() { 
     //this.name = 'Felipe'; // setta il nome nel costruttore
   }
